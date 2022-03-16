5c91440ed707b0e6bfd81b9415b8a28e74c37cb9
audreyfcz <fcz.audrey@gmail.com>
2022-03-16 10:14:17 +0100

    display comment admin

--- Views/admin/comments.php
+++ Views/admin/comments.php
@@ -4,28 +4,42 @@
 <h6 class="text-uppercase mt-5">Comments list: </h6>
 <hr>
 <?php foreach ($comments as $comment) : ?>
-        <div class="card comment mb-2">
-            <div class="container mt-1">
-                <div class="d-flex justify-content-between px-1">
-                    <div class="container mt-3">
-                        <p><?= date("d/m/Y H:i", strtotime($comment->comment_date)); ?></p>
-                        <p class="d-flex justify-content-start">
-                            <?= $comment->comment ?>
-                        </p>
-                    </div>
-                    <!--<div class="d-flex row-flex">
-                        <form action="<?/*= URL */?>/comments/check/<?/*= $comment->comment_id */?>" method="POST">
+    <div class="card comment mb-2">
+        <div class="container mt-1">
+            <div class="row px-1">
+                <div class="col-md-10">
+                    <p><?= date("d/m/Y H:i", strtotime($comment->comment_date)); ?></p>
+                    <p>
+                        <?= $comment->comment ?>
+                    </p>
+                </div>
+                <div class="col-md-2 align-self-center ">
+                    <div class="d-flex flex-row-reverse">
+                        <form action="<?= URL  ?>/comments/delete/<?= $comment->comment_id ?>" method="POST">
+                            <button type="submit" value="" class="btn red a-red-tag">
+                                <i class="fa-solid fa-xmark" aria-hidden="true"></i>
+                            </button>
+                        </form>
+                        <form action="<?=  URL  ?>/comments/check/<?= $comment->comment_id ?>" method="POST">
                             <button type="submit" value="" class="btn color a-tag">
                                 <i class="fa fa-check" aria-hidden="true"></i>
                             </button>
                         </form>
-                        <form action="<?/*= URL */?>/comments/delete/<?/*= $comment->comment_id */?>" method="POST">
+                    </div>
+                </div>
+                <!--<div class="d-flex row-flex">
+                        <form action="<? /*= URL */ ?>/comments/check/<? /*= $comment->comment_id */ ?>" method="POST">
+                            <button type="submit" value="" class="btn color a-tag">
+                                <i class="fa fa-check" aria-hidden="true"></i>
+                            </button>
+                        </form>
+                        <form action="<? /*= URL */ ?>/comments/delete/<? /*= $comment->comment_id */ ?>" method="POST">
                             <button type="submit" value="" class="btn red a-red-tag">
                                 <i class="fa-solid fa-xmark" aria-hidden="true"></i>
                             </button>
                         </form>
                     </div>-->
-                </div>
             </div>
         </div>
+    </div>
 <?php endforeach; ?>
