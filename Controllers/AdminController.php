38754b28c0ce89ebb45e7756020b9f66b81c37cc
audreyfcz <fcz.audrey@gmail.com>
2022-04-09 15:51:52 +0200

    rebase

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -58,4 +58,36 @@ class AdminController extends Controller
             exit;
         }
     }
+
+    public function validComment(int $idComment)
+    {
+        // requête de validation des commentaires
+        if ($this->isAdmin()) {
+            $commentModel = new CommentsModel();
+            $commentArray = $commentModel->find($idComment);
+
+            if ($commentArray) {
+                $comment = $commentModel->hydrate($commentArray);
+
+                $comment->setIsValid($comment->getIsValid() ? 0 : 1);
+                $comment->update();
+
+                header('Location: ../../admin/comments/' . $comment->getPostId());
+            }
+        }
+    }
+
+    /**
+     * Supprimer le commentaire
+     * @param int $idComment
+     * @return void
+     */
+    public function delete(int $idComment)
+    {
+        if ($this->isAdmin()) {
+            $comment = new CommentsModel();
+            $comment->delete($idComment);
+            header('Location: ../../admin');
+        }
+    }
 }
